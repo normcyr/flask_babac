e24c118f86a7927a15b4c4c51344ff2a16383f29
Normand Cyr <norm@normandcyr.com>
2020-04-15 15:54:45 -0400

    version 0.1.0
    - enable proper command line usage of `flask_babac`
      - deal with empty results
      - load the same username/password configuration used for the web front-end
    - use raw string notation for regular expressions when using backslashes
    - handle empty search results
    - have error messages in HTML page displayed in red
    - searching animation now disappears after second search
    
    - results table does not disappear immediately when a new search is performed

--- flask_babac/flask_babac.py
+++ flask_babac/flask_babac.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from recherche_babac2 import recherche_babac2 as rb2
-from flask_babac import settings, _version
+import re
 import argparse
+from flask_babac import settings, _version
+from recherche_babac2 import recherche_babac2 as rb2
+
+
+sku_pattern = re.compile(
+    r"\d{2}[-]?\d{3}$"
+)  # accept 12-345 or 12345, but not 123456 or 1234
+text_pattern = re.compile(
+    r"[\w0-9 \-]+"
+)  # accept text, numbers, but no special character except -
+price_pattern = re.compile(r"\d*[.]\d{2}")
 
 
 def do_the_search(username_babac, password_babac, search_text):
 
-    if username_babac != None or password_babac != None:
+    if username_babac is not None or password_babac is not None:
         recherche = rb2.BabacSearch(username_babac, password_babac)
-        list_products, loggedin = recherche.do_the_search(search_text)
+        (
+            list_products,
+            loggedin,
+            multiple_pages,
+            item_page_url,
+        ) = recherche.do_the_search(search_text)
 
         if loggedin:
-            return list_products
+            return list_products, multiple_pages, item_page_url
         else:
             print(
                 "The username and/or password is incorrect. Please verify your login information in the configuration file."
@@ -42,12 +57,24 @@ def main():
     args = parser.parse_args()
 
     search_text = " ".join(args.search_text)
-    print("Searching for: '{}'".format(search_text))
 
-    username_babac, password_babac = settings.read_config()
-    list_products = do_the_search(username_babac, password_babac, search_text)
+    if re.match(sku_pattern, search_text) or re.match(text_pattern, search_text):
+        print("Searching for: '{}'".format(search_text))
 
-    rb2.print_results(list_products)
+        username_babac, password_babac = settings.read_config()
+        recherche = rb2.BabacSearch(username_babac, password_babac)
+        (
+            list_products,
+            loggedin,
+            multiple_pages,
+            item_page_url,
+        ) = recherche.do_the_search(search_text)
+        if loggedin:
+            rb2.print_results(list_products, multiple_pages, item_page_url)
+        else:
+            print("Failed login.")
+    else:
+        print("Please avoid using special characters.")
 
 
 if __name__ == "__main__":
